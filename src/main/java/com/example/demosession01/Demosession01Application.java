fa77db35088d50a6d61c296802c2487058722a45
vernetto <Ernesto0z>
2020-08-10 11:08:33 +0200

    first commit

---
+++ src/main/java/com/example/demosession01/Demosession01Application.java
@@ -0,0 +1,13 @@
+package com.example.demosession01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demosession01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demosession01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosession01/Demosession01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosession01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosession01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
